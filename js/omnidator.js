7c53e32fa0b7c3f1bb0bfbf3a737c359f8e14d63
mhausenblas <michael.hausenblas@deri.org>
2011-06-14 16:04:12 +0100

    updated about and added example translations

--- js/omnidator.js
+++ js/omnidator.js
@@ -4,10 +4,12 @@ $(function(){
 	
 	$("#doTranslate").click(function() {
 		//lowlevelAPICall();
+		$("#result").html("");
+		$("#resultcontainer").slideUp();
 		mainAPICall();
 	});
-	
-	
+
+
 	// menu selection
 	$("#menu span").click(function() {
 		$("#menu span").each(function(index) {
@@ -17,6 +19,24 @@ $(function(){
 		$(this).addClass('active');
 		$('#' + $(this).attr('id') + '-content').show();
 		window.location =  '#' + $(this).attr('id');
+	});	
+	
+	// example selection
+	$(".translate-examples").click(function() {
+		var ex = $(this).text();
+		
+		$("#result").html("");
+		$("#resultcontainer").slideUp();
+		if(ex == "CSV to JSON"){
+			$("#from").val("csv");
+			$("#to").val("json");
+			$("#docURL").val("http://omnidator.appspot.com/examples/solar-system.csv");
+		}
+		if(ex == "Microdata to RDF/Turtle"){
+			$("#from").val("microdata");
+			$("#to").val("rdf-turtle");
+			$("#docURL").val("http://omnidator.appspot.com/examples/md-test-1.html");
+		}
 	});
 });
 
@@ -29,18 +49,19 @@ function mainAPICall() {
 		type: "GET",
 		dataType: 'text',
 		data: "url=" + escape($("#docURL").val()),
-		success : function(data) {
+		success : function(data, textStatus, errorThrown) {
 			if(data != 'None'){
 				$("#result").html(htmlEntities(data));
 			}
 			else {
-				$("#result").html("Some error prevented me to show you the result. Blame <a href='http://sw-app.org/mic.xhtml'>Michael</a> ...")
+				$("#result").html("Either you selected the wrong translation parameters (from/to) or the input is crap.");
 			}
 			$("#resultcontainer").slideDown();
 		
 		},
-		error: function(xhr, textStatus, errorThrown){
-			alert("Sorry, there was an error: " + textStatus);
+		error: function(data, textStatus, errorThrown){
+			$("#result").html("Some error prevented me to show you the result. I got a " +  data.status + " - sorry, not much we can do about it ;)");
+			$("#resultcontainer").slideDown();
 		}
 	});
 }
